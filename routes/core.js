9dca0d361997abd32c16c0ef1739219e0a74606a
James Cordeiro <james@cordeiro.co.vu>
2015-07-30 01:02:00 +0100

    Changed core routes file

--- routes/core.js
+++ routes/core.js
@@ -1,8 +1,7 @@
 var 	express		=		require('express'),
 		app 		=		express(),
 		router		=		express.Router(),
-		bodyParser 	= 		require('body-parser'),
-		msql		=		require('../my_modules/msql');
+		bodyParser 	= 		require('body-parser');
 
 
 
@@ -18,34 +17,15 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 
-msql = new msql({
-	schema: "acca_members"
-});
-msql.connect();
 
 /* -----------------------     Routes Logic    -------------------------- */
 
 router.route('/')
 
 	.get(app, function(req, res) {
-
-		msql.getSchema(function(data){
-			console.log(data);
-		});
-
-
-
 		res.render("app", staticLocals);
 	});
 
-
-router.route('/mapping')
-
-	.post(app, function(req, res) {
-		msql.getData(msql.parseData(req.body), function(data){
-			res.json(data);
-        });
-	});
 
 
 
